7adf604ded5ca16695c643ec4d0798be691976ec
Ruben Martinez <ruben.martinez1@gmail.com>
2019-11-22 12:19:12 +0100

    First version of the script and minimal documentation.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# helm-chart-requirements-updater
+# Helm Chart Requirements Updater
 A command to update the requirements in your chart to the latest available option
+
+## Usage
+
+`requirements-updater` is able to parse the output given by `helm search`, using that information to update the requirements in the Helm chart provided as argument:
+
+```bash
+helm repo update
+helm search | helm-requirements-updater.py ./mychart
+```
+
+## Pending features
+
+Providing `requirements-updater` as a Helm plugin.
---
+++ helm-requirements-updater.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import re
+import sys
+import os
+import yaml
+
+pattern = re.compile("^([0-9\w\-_\.]+/[0-9\w\-_\.]+)\s+([0-9\w\-_\.]+).*$")
+
+class Chart:
+    def __init__(self, name, repo, version):
+        self.name = name
+        self.repo = repo
+        self.version = version
+
+charts = {}
+
+if len(sys.argv) == 1:
+    print("Expected argument: path to helm chart")
+    sys.exit(1)
+
+#import subprocess
+#completed_process = subprocess.run(["helm", "search"]) #, capture_output=True)
+#for line in completed_process.stdout.splitlines():
+for line in sys.stdin:
+    m = pattern.match(line)
+    if m:
+        (chart, version) = m.groups()
+        (repo, name) = chart.split("/")
+        charts[name] = Chart(name, repo, version)
+
+with open(os.path.join(sys.argv[1], "requirements.yaml")) as f:
+    requirements = yaml.load(f)
+
+for dependency in requirements["dependencies"]:
+    if dependency["name"] in charts.keys():
+        chart = charts[dependency["name"]]
+        if dependency["repository"].replace('@', '') == chart.repo:
+            dependency["version"] = chart.version
+
+with open(os.path.join(sys.argv[1], "requirements.yaml"), "w") as f:
+    yaml.dump(requirements, f, default_flow_style=False)
